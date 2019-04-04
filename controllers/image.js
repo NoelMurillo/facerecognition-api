9be98b3e81a9537cfdd9bba4901f9499acf4935f
LAPTOP-6JT3A2QV\noel2 <noel25_1594@hotmail.com>
2019-04-04 12:22:53 -0600

    Changes made for security

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,20 @@
+const clarifai = require('clarifai');
 
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: '49c55de0a25645e9823334cace3326c0'
+});
+
+
+const handleApiCall = (req, res) =>{
+  app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      req.body.input)
+      .then(data =>{
+        res.json(data);
+      })
+      .catch(err=> res.status(400).json('Unable to connect with API'))
+}
 
 const handleImage = (req, res, db)=> {
   const {id} = req.body;
@@ -12,5 +28,6 @@ const handleImage = (req, res, db)=> {
 }
 
 module.exports= {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 }
--- server.js
+++ server.js
@@ -31,12 +31,10 @@ app.get('/', (req, res)=>{
 });
 
 app.post('/signin', (req, res)=>{ signin.handleSignin(req, res, db, bcrypt)} );
-
 app.post('/register',  (req, res)=>{ register.handleRegister(req, res, db, bcrypt)} );
-
 app.get('/profile/:id', (req, res)=>{ profile.handleProfile(req, res, db)} );
-
 app.put('/image', (req, res)=>{ image.handleImage(req, res, db ) });
+app.post('/imageUrl', (req, res)=>{ image.handleApiCall(req, res) });
 
 app.listen(process.env.PORT || 3000, ()=>{
   console.log(`App running on port ${process.env.PORT}`);
